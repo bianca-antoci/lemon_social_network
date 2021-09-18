aa5925cc1589a11c2ec3d38c502dfefa10fef0ba
Bianca Antoci <bianca.antoci96@gmail.com>
2021-09-18 21:39:57 +0100

    chore: added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './features/home/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /**
+   * The internal routing.
+   * It requires the user to be logged in.
+   */
+  { path: 'home', component: HomePageComponent },
+  { path: 'recipies', component: HomePageComponent },
+  { path: 'diets', component: HomePageComponent },
+  { path: 'products', component: HomePageComponent },
+  { path: 'profile', component: HomePageComponent },
+  { path: 'notifications', component: HomePageComponent },
+  { path: 'new-post', component: HomePageComponent },
+
+  /**
+   * The external routing.
+   * It doesn't require a session.
+   */
+  { path: 'welcome', component: HomePageComponent },
+  { path: 'welcome/register', component: HomePageComponent },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lemon-social-network';
+  isLoggedIn = false;
+  
+  constructor(private router: Router) {
+    // we listen for route changes to detect if user still logged in
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+       this.isLoggedIn = val.url.indexOf('welcome') === -1;
+      }
+  });
+  }
 }
